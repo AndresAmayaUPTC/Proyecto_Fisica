d82196f69d92a06d265f35792dd3e7217d087133
Andres felipe Puentes <andres.puentes02@uptc.edu.co>
2023-09-18 16:15:54 -0500

    carpeta

---
+++ src/Metodos.java
@@ -0,0 +1,10 @@
+public class Metodos {
+
+    public double calcularperiodoFormula(double tiempo,double nvueltas){ 
+     return tiempo/nvueltas;
+    }
+     public double calcularperiodoFormuladespeje(){
+     double x=0;
+     return x;
+    }
+}
