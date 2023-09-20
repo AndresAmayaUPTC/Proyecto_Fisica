633c3cde6acba59541525f073e082831287e3e6e
Andres felipe Puentes <andres.puentes02@uptc.edu.co>
2023-09-19 20:58:36 -0500

    botones

--- src/Interfaz/GUIPeriodo.java
+++ src/Interfaz/GUIPeriodo.java
@@ -4,9 +4,14 @@ package Interfaz;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 class GUIPeriodo extends JFrame{
+
+  private JButton btnFormula1, btnFormula2, btnFormula3;
+  private JTextField descripcion1,descripcion2,descripcion3;
 
   public GUIPeriodo() {
         setTitle("Periodo");
@@ -36,7 +41,13 @@ class GUIPeriodo extends JFrame{
     }
 
     private void beginComponents() {
-        
+      btnFormula1 = new JButton("Ingresar Datos");
+		btnFormula2 = new JButton("Ingresar Datos");
+		btnFormula3 = new JButton("Ingresar Datos"); 
+    
+    descripcion1= new JTextField("Calcular periodo con la formula general");
+     descripcion2= new JTextField("Calcular periodo despejando la formula de velocidad angular");
+      descripcion3= new JTextField("Calcular periodo despejando la formula de velocidad lineal");
     }
     
 }
