f48be8e7efeefb62643626c859100e7c1325ebad
Andres Amaya <andres.amaya03@uptc.edu.co>
2023-09-19 21:24:54 -0500

    Boton volver a la ventana de periodo agregado

--- src/Interfaz/GUIPeriodo.java
+++ src/Interfaz/GUIPeriodo.java
@@ -1,5 +1,4 @@
 
-
 package Interfaz;
 
 import java.awt.BorderLayout;
@@ -10,56 +9,68 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class GUIPeriodo extends JFrame{
+public class GUIPeriodo extends JFrame {
 
-  private JButton btnFormula1, btnFormula2,btnFormula3;
-  private JTextField descripcion1,descripcion2,descripcion3;
+  private JButton btnVolver;
+  private JButton btnFormula1, btnFormula2, btnFormula3;
+  private JTextField descripcion1, descripcion2, descripcion3;
 
   public GUIPeriodo() {
-        setTitle("Periodo");
-		setSize(800, 600);
-		setLayout(new BorderLayout());
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setResizable(true);
+    setTitle("Periodo");
+    setSize(800, 600);
+    setLayout(new BorderLayout());
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setResizable(true);
 
-		begin();
+    begin();
 
-		setVisible(true);
+    setVisible(true);
 
-        
-    }
+  }
 
-    public void begin() {
+  public void begin() {
 
-            beginComponents();
-    
-            addComponents();
-    
-        }
+    beginComponents();
 
-    private void addComponents() {
-       JPanel panel = new JPanel();
-       panel.setLayout(new GridLayout(2, 2));
-        
-       panel.add(descripcion1);
-       panel.add(descripcion2);
-        panel.add(descripcion3);
-       panel.add(btnFormula1);
-       panel.add(btnFormula2);
-       panel.add(btnFormula3);
-       
-        this.add(panel, BorderLayout.SOUTH);
+    addComponents();
 
-    }
+  }
 
-    private void beginComponents() {
+  private void addComponents() {
+
+    this.add(btnVolver, BorderLayout.SOUTH);
+
+
+        btnVolver.addActionListener((e) -> {
+			new MainMenu();
+            dispose();
+        });
+
+
+    JPanel panel = new JPanel();
+    panel.setLayout(new GridLayout(2, 2));
+
+    panel.add(descripcion1);
+    panel.add(descripcion2);
+    panel.add(descripcion3);
+    panel.add(btnFormula1);
+    panel.add(btnFormula2);
+    panel.add(btnFormula3);
+
+    this.add(panel, BorderLayout.NORTH);
+
+  }
+
+  private void beginComponents() {
+
+    btnVolver = new JButton("Volver");
     btnFormula1 = new JButton("Ingresar Datos");
-		btnFormula2 = new JButton("Ingresar Datos");
-		btnFormula3 = new JButton("Ingresar Datos"); 
-    
-    descripcion1= new JTextField("Calcular periodo con la formula general");
-     descripcion2= new JTextField("Calcular periodo despejando \nla formula de velocidad angular");
-      descripcion3= new JTextField("Calcular periodo despejando\n la formula de velocidad lineal");
-    }
-    
+    btnFormula2 = new JButton("Ingresar Datos");
+    btnFormula3 = new JButton("Ingresar Datos");
+
+    descripcion1 = new JTextField("Calcular periodo con la formula general");
+    descripcion2 = new JTextField("Calcular periodo despejando \nla formula de velocidad angular");
+    descripcion3 = new JTextField("Calcular periodo despejando\n la formula de velocidad lineal");
+  }
+
 }
